0877fc6748c208be8e7f402533975aaa7468304e
rafaeltecfac <rafael.frnaco-diaz@tecfac.com>
2024-04-08 15:16:01 -0400

    commit

--- Javascript/empty.js
+++ Javascript/empty.js
@@ -1,1 +1,14 @@
 //empty file
+
+class Empty_Object{
+	constructor(fname, lname, job){
+		var s = this;
+		s.fname = fname;
+		s.lname = lname;
+		s.job = job;
+
+	}//end of constructor 
+
+
+
+}//end of class 
